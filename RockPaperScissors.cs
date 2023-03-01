2ffc6642fa60deff6da26149ff298ab630c4fc63
staykom <staykomatev@gmail.com>
2023-03-01 09:50:56 +0200

    Updated version

--- RockPaperScissors.cs
+++ RockPaperScissors.cs
@@ -1,18 +1,82 @@
 ﻿using System;
+using System.Drawing;
+using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RockPaperScissors
 {
     internal class RockPaperScissors
     {
+
         static void Main(string[] args)
         {
 
+            {
+
+                bool isValid = true;
+
+                while (isValid)
+                {
+                    RockPaperScissorsMethod();
+
+                    string cmd = string.Empty;
+
+                    while (cmd != "y")
+                    {
+
+                        cmd = Console.ReadLine().ToLower();
+                        Console.WriteLine();
+
+                        if (cmd == "y")
+                        {
+                            continue;
+                        }
+
+                        else if (cmd == "n")
+                        {
+                            isValid = false;
+                            Environment.Exit(0);
+                        }
+
+                        else
+                        {
+                            while (cmd != "y" && cmd != "n")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("Invalid Input. Try Again...");
+                                Console.ResetColor();
+
+                                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                                Console.Write($"\nAnother Game?\n[y]es or [n]o");
+                                Console.ResetColor();
+                                Console.WriteLine();
+                                break;
+
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        private static void RockPaperScissorsMethod()
+        {
             const string Rock = "Rock";
             const string Paper = "Paper";
             const string Scissors = "Scissors";
 
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.Write("Choose [r]ock, [p]aper or [s]scissors: ");
+            Console.ResetColor();
+
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
             string playerMove = Console.ReadLine().ToLower();
+            Console.ResetColor();
 
             if (playerMove == "r" || playerMove == "rock")
             {
@@ -28,7 +92,14 @@ namespace RockPaperScissors
             }
             else
             {
-                Console.WriteLine("Invalid Input. Try Again...");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nInvalid Input. Try Again...");
+                Console.ResetColor();
+
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Write($"\nAnother Game?\n[y]es or [n]o\n");
+                Console.ResetColor();
+
                 return;
             }
             Random random = new Random();
@@ -49,29 +120,42 @@ namespace RockPaperScissors
                     break;
 
             }
-            Console.WriteLine($"The Computer chose: {computerMove}.");
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"\nThe Computer chose: {computerMove}.\n");
+            Console.ResetColor();
 
             if ((playerMove == Rock && computerMove == Scissors) ||
                 (playerMove == Paper && computerMove == Rock) ||
                 (playerMove == Scissors && computerMove == Paper))
             {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("You Win!");
+                Console.ResetColor();
             }
 
             else if ((playerMove == Rock && computerMove == Paper) ||
                 (playerMove == Paper && computerMove == Scissors) ||
                 (playerMove == Scissors && computerMove == Rock))
             {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("You Lose!");
+                Console.ResetColor();
             }
 
-            else 
+            else
             {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("This Game Was A Draw!");
+                Console.ResetColor();
             }
 
-
-
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write($"\nAnother Game?\n[y]es or [n]o\n");
+            Console.ResetColor();
         }
     }
 }
